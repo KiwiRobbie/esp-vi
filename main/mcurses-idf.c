223bc41192d91fbf468208a1b9cfbe48e90d5863
KiwiRobbie <r.b.christie@icloud.com>
2024-09-10 21:25:05 +1200

    vi

--- main/mcurses-idf.c
+++ main/mcurses-idf.c
@@ -14,6 +14,8 @@
 #include "tusb_cdc_acm.h"
 #include "sdkconfig.h"
 #include "mcurses.h"
+#include "esp_random.h"
+#include "cl_main.h"
 
 #define PI 3.141592653589793
 #define VT100_moveTo(x, y) move(y, x)
@@ -92,8 +94,48 @@ void esp_putchar(uint8_t c)
     tinyusb_cdcacm_write_queue_char(0, c);
 }
 
+int random_int(int start, int stop)
+{
+
+    uint32_t range = stop - start + 1;
+    uint32_t offset = (esp_random() & INT_MAX) % range;
+    return start + offset;
+}
+
+void sub_d(int p, int s, int x, int y)
+{
+    uint8_t r;
+    uint8_t g;
+
+    r = (p % 16) * 16;
+    g = 180 - p;
+    if (r < 10)
+        attrset(F_BLACK); // forground color black
+    else
+    {
+        if (g > 170)
+            attrset(F_WHITE);
+        else if (g > 169)
+            attrset(F_GREEN);
+    }
+
+    if ((y >= 0) && (y < 24) && (x < 80))
+    {
+        int xx, yy;
+        xx = x;
+        yy = y;
+        VT100_moveTo(xx, yy);
+        char c;
+        c = 33 + (x * y) % 200;
+        addch(c);
+    }
+}
+
+int t[80];
+
 void app_main(void)
 {
+
     // Create FreeRTOS primitives
     app_queue = xQueueCreate(5, sizeof(app_message_t));
     assert(app_queue);
@@ -141,6 +183,17 @@ void app_main(void)
 #endif
 
     ESP_LOGI(TAG, "USB initialization DONE");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    ESP_LOGI(TAG, "Waiting to start VI main()");
+
+    vTaskDelay(3000 / portTICK_PERIOD_MS);
+
+    char **argv = {"vi\0"};
+
+    main(1, argv);
+    ESP_LOGI(TAG, "VI main() returned");
+
+    vTaskDelay(3000 / portTICK_PERIOD_MS);
     setFunction_putchar(esp_putchar); // tell the library which output channel shall be used
 
     {
@@ -149,22 +202,63 @@ void app_main(void)
         ESP_LOGI(TAG, "===================");
         ESP_LOGI(TAG, "VT100 graphics demo");
         ESP_LOGI(TAG, "===================");
-        vTaskDelay(3000 / portTICK_PERIOD_MS);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        for (int i = 0; i < 80; i++)
+        {
+            esp_putchar(random_int('a', 'z'));
+            vTaskDelay(10 / portTICK_PERIOD_MS);
+        }
+        tinyusb_cdcacm_write_flush(0, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
         curs_set(0); // set cursor invisible
         clear();
     }
     while (1)
     {
-        static float n = 0;
-        float x, y;
 
-        x = (sin(2 * PI * n / POINTS) * RADIUS + XPOS) * SCALEX;
-        y = cos(2 * PI * n / POINTS) * RADIUS + YPOS;
-        n++;
-        VT100_moveTo(x, y);
-        fputc('+', stdout);
+        int x = 0;
+        int y = 0;
 
-        vTaskDelay(100 / portTICK_PERIOD_MS);
+        for (int i = 0; i < 80; i++)
+        {
+            t[i] = random_int(-50, 0);
+        }
+
+        // sub_d(1,1,10,10);
+        while (1)
+        {
+            for (int k = 1; k < 80; k++)
+            {
+                int i = random_int(0, 79);
+                if (t[i] > 28)
+                    t[i] = 0;
+                t[i] = t[i] + 1;
+                y = t[i];
+                sub_d(0, 0, i, y - 6);
+                sub_d(2 + x, 0, i, y - 5);
+                sub_d(2 + x, 0, i, y - 4);
+                sub_d(10 + x, 0, i, y - 3);
+                sub_d(10 + x, 0, i, y - 2);
+                sub_d(11 + x, 0, i, y - 1);
+                sub_d(0, 2 + x, i, y);
+            }
+            vTaskDelay(30 / portTICK_PERIOD_MS);
+        }
+
+        // {
+        //     static float n = 0;
+        //     float x, y;
+
+        //     x = (sin(2 * PI * n / POINTS) * RADIUS + XPOS) * SCALEX;
+        //     y = cos(2 * PI * n / POINTS) * RADIUS + YPOS;
+        //     n++;
+        //     VT100_moveTo(x, y);
+        //     fputc('+', stdout);
+
+        //     vTaskDelay(100 / portTICK_PERIOD_MS);
+        // }
+
         // if (xQueueReceive(app_queue, &msg, portMAX_DELAY))
         // {
         //     if (msg.buf_len)
